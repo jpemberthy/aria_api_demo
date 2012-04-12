a8daf6726defbf38365442129247aca4c8ee77d7
jpemberthy <jpemberthy@gmail.com>
2012-04-12 10:53:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AriaDemo::Application.config.secret_token = '3de27bb9616c1801d0f5fc2ef597bc548a2ddbabd2b73faa842e1efd812118e316a5da67ba5afb6253eeaeb4798bebc3cafeb4bf216077b3736fac78e9f60d33'
